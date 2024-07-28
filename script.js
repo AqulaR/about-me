35d98d98c7c67f5150af1b19104b68eddceba090
AqulaR <103633654+AqulaR@users.noreply.github.com>
2024-07-28 14:39:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("copyright").innerHTML = `© Nickolay Ashapatov | ${new Date().getFullYear()}`;
+document.getElementById("name_age").innerHTML = `Николай, ${(new Date().getFullYear() - 2004)} лет`;
